490b5d87f7ef3a541fbf6bec81bf74cd36840ae7
feilongproject <flzs6137@gmail.com>
2021-10-10 21:37:46 +0800

    大改样式（仿洛谷风格）

--- index.php
+++ index.php
@@ -14,69 +14,61 @@
         if(type == "c_cpp"){
 		var res=await fetch("<?php echo $ApiUrl?>?code="+encodeURIComponent(code)+"&cin="+encodeURIComponent(cin));
           var data = await res.text();
-          $("#output").html("<pre class=\"fillall\">" + data.replace(/</g,"<").replace(/>/g,">").replace(/\n/g,"<br>") + "</pre>");
+          $("#cout").html("<pre class=\"fillall\">" + data.replace(/</g,"<").replace(/>/g,">").replace(/\n/g,"<br>") + "</pre>");
         }
 
       }
     </script>
     <style type="text/css" media="screen">
       body {background-color: #2D2F32;color: #fff;}
-      #editor {margin: 0;position: absolute;top: 0;bottom: 0;left: 0;right: 0;}
-      #cin {margin: 0;height:100px;top: 0;bottom: 0;left: 0;right: 0;}
+      #editor {margin: 0;height:100%;position: absolute;top: 5%;bottom: 0;left: 0;right: 0;}
+      .inout{margin: 0;height:100%;width:100% ;position: absolute;top: 20%}
+      #cin {margin: 0;height:50%;width:50% ;position: absolute;top: 20%;bottom: 0;left: 0;right: 0;}
+      #cout {margin: 0;height:50%;width:50% ;position: absolute;top: 20%;bottom: 0;left: 50%;right: 0;}
       .container {margin: 0;top: 0;bottom: 0;}
-      #editordiv {margin: 0;position: absolute;top: 0;bottom: 0;left:0;right:58.33333333333334%;}
-      #iframediv {margin: 0;position: absolute;top: 0;bottom: 50%;left: 41.66666666666667%;right:16.66666666666667%;}
-      #stepdiv {margin: 0;position: absolute; top: 50%; bottom: 0; left: 41.66666666666667%; right:16.66666666666667%;}
-      .col-md-2 {margin: 0;position: absolute;top: 0;bottom: 0;left: 83.33333333333334%;right:0;}
+      #editordiv {height: 75%;margin: 0;position: absolute;top: 0;bottom: 0;left:0;right:0%;}
+      #iframediv {margin: 0;position: absolute;top: 80%;bottom: 0%;left: 0%;right: 0%;}
+      .col-md-2 {margin: 0;position: absolute;top: 0;bottom: 0;left: 83.333%;right:0;}
+      .form-control {width: auto;}
+      .command-group {margin: 0;height:50%;width:100% ;position: absolute;top: 0%;bottom: 0;left: 0;right: 0;} 
     </style>
   </head>
   <body>
-  <div>
-    <div class="container">
-      <div class="col-md-5 column" id="editordiv">
-        <pre id="editor"></pre>
-      </div>
+    <div>
+      <div class="container">
+	<h3>SimpleWebIDE - 
+          <a href="https://feilongproject.com/">飞龙project</a>
+          GitHub：<a href="https://github.com/feilongproject/SimpleWebIde">GitHub</a>
+        </h3>
+        <div class="editer-form">
+          <div class="col-md-5 column" id="editordiv">
+            <pre id="editor"></pre>
+          </div>
+        </div>
       <div class="col-md-5 column" id="iframediv">
-	  <pre id="cin"></pre>
-          <h3>运行结果：</h3>
-        <div id="output"></div>
-      </div>
-      <div class="col-md-5 column" id="stepdiv">
-	<h3 id="stepcount">SimpleWebIDE</h3>
-        <h4 id="steptext">作者：<a href="https://feilongproject.com/">飞龙project</a></h4>
-        <h4 id="steptext">GitHub：<a href="https://github.com/feilongproject/SimpleWebIde">GitHub</a></h4>
-        <!-- 更改语言-start -->
-        <div class="form-group">
+        <div class="inout">
+          <pre id="cin"></pre>
+          <pre id="cout"></pre>
+        </div>
+
+        <div class="command-group">
           <select name="language" id="language" class="form-control">
             <option value="c_cpp" selected>C++（.cpp）</option>
           </select>
-
-          <button id="changelang" class="btn btn-default">
-            更改语言
-          </button>
-        </div>
-        <!-- 更改语言-end -->
-        <br>
-        <!-- 更改皮肤-end -->
-        <div class="form-group">
+          <button id="changelang" class="btn btn-default">更改语言</button>
           <select name="skin" id="skin" class="form-control">
               <?php require("./skin.html")?>
 	  </select>
           <button id="changeskin" class="btn btn-default">更改皮肤</button>
-        </div>
-        <!-- 更改语言-end -->
-        <button class="btn btn-default" id="cheak">
-          <span class="glyphicon glyphicon-play-circle"></span>运行代码
-        </button>
-        <br><br>
-        <div class="form-group">
+          <button class="btn btn-default" id="cheak">
+            <span class="glyphicon glyphicon-play-circle"></span>运行代码
+          </button>
           <button class="btn btn-default" id="savecode">
             <span class="glyphicon glyphicon-save"></span>保存代码（通过Cookie）
           </button>
           <button class="btn btn-default" id="readcode">
             <span class="glyphicon glyphicon-open"></span>读入代码（通过Cookie）
           </button>
-        </div>
       </div>
     </div>
     <script src="src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
@@ -85,6 +77,11 @@
       cin.setOptions({enableLiveAutocompletion: true});
       cin.setTheme("ace/theme/tomorrow_night");
       cin.session.setMode("ace/mode/c_cpp");
+
+      var cout = ace.edit("cout");
+      cout.setOptions({enableLiveAutocompletion: true});
+      cout.setTheme("ace/theme/tomorrow_night");
+      cout.session.setMode("ace/mode/c_cpp");
 
       var editor = ace.edit("editor");
       editor.setOptions({enableLiveAutocompletion: true});
