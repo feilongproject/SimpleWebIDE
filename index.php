0c6a0744dacc6631e1036f0b1dacf55629f870f4
feilongproject <flzs6137@gmail.com>
2021-10-10 16:22:57 +0800

    修改背景; 主题设置简洁化; 部分地方采用设置文件;

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en">
+<?php include("./config.php")?>
+<!DOCTYPE HTML>
+<html lang="zh">
   <head>
     <meta charset="UTF-8">
-    <title>FredTools IDE</title>
+    <title>SimpleWebIDE - 飞龙project</title>
     <link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdn.staticfile.org/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
@@ -11,7 +12,7 @@
     async function runcode(code,cin,type){
         console.log(code,type);
         if(type == "c_cpp"){
-          var res=await fetch("http:///ide/api.php?code="+encodeURIComponent(code)+"&cin="+encodeURIComponent(cin));
+		var res=await fetch("<?php echo $ApiUrl?>?code="+encodeURIComponent(code)+"&cin="+encodeURIComponent(cin));
           var data = await res.text();
           $("#output").html("<pre class=\"fillall\">" + data.replace(/</g,"<").replace(/>/g,">").replace(/\n/g,"<br>") + "</pre>");
         }
@@ -19,6 +20,7 @@
       }
     </script>
     <style type="text/css" media="screen">
+      body {background-color: #2D2F32;color: #fff;}
       #editor {margin: 0;position: absolute;top: 0;bottom: 0;left: 0;right: 0;}
       #cin {margin: 0;height:100px;top: 0;bottom: 0;left: 0;right: 0;}
       .container {margin: 0;top: 0;bottom: 0;}
@@ -40,11 +42,9 @@
         <div id="output"></div>
       </div>
       <div class="col-md-5 column" id="stepdiv">
-        <h3 id="stepcount">自由模式</h3>
-        <p id="steptext">在此模式下，你可以自由的使用FredTools IDE。</p>
-        <p id="task">任务：无</p>
-      </div>
-      <div class="col-md-2 column">
+	<h3 id="stepcount">SimpleWebIDE</h3>
+        <h4 id="steptext">作者：<a href="https://feilongproject.com/">飞龙project</a></h4>
+        <h4 id="steptext">GitHub：<a href="https://github.com/feilongproject/SimpleWebIde">GitHub</a></h4>
         <!-- 更改语言-start -->
         <div class="form-group">
           <select name="language" id="language" class="form-control">
@@ -60,42 +60,7 @@
         <!-- 更改皮肤-end -->
         <div class="form-group">
           <select name="skin" id="skin" class="form-control">
-<option value='chrome'>Chrome</option>
-<option value='clouds'>Clouds</option>
-<option value='crimson editor'>Crimson Editor</option>
-<option value='dawn'>Dawn</option>
-<option value='dreamweaver'>Dreamweaver</option>
-<option value='eclipse'>Eclipse</option>
-<option value='github'>GitHub</option>
-<option value='iplastic'>IPlastic</option>
-<option value='solarized light'>Solarized Light</option>
-<option value='textmate'>TextMate</option>
-<option value='tomorrow'>Tomorrow</option>
-<option value='xcode'>XCode</option>
-<option value='kuroir'>Kuroir</option>
-<option value='katzenmilch'>KatzenMilch</option>
-<option value='sqlserver'>SQL Server</option>
-<option value='ambiance'>Ambiance</option>
-<option value='chaos'>Chaos</option>
-<option value='clouds_midnight'>Clouds Midnight</option>
-<option value='cobalt'>Cobalt</option>
-<option value='gruvbox'>Gruvbox</option>
-<option value='gob'>Green on Black</option>
-<option value='idle_fingers'>idle Fingers</option>
-<option value='kr_theme'>krTheme</option>
-<option value='merbivore'>Merbivore</option>
-<option value='merbivore_soft'>Merbivore Soft</option>
-<option value='mono_industrial'>Mono Industrial</option>
-<option value='monokai'>Monokai</option>
-<option value='pastel_on_dark'>Pastel on dark</option>
-<option value='solarized_dark'>Solarized Dark</option>
-<option value='terminal'>Terminal</option>
-<option value='tomorrow_night'>Tomorrow Night</option>
-<option value='tomorrow_night_blue'>Tomorrow Night Blue</option>
-<option value='tomorrow_night_bright'>Tomorrow Night Bright</option>
-<option value='tomorrow_night_eighties'>Tomorrow Night 80s</option>
-<option value='twilight'>Twilight</option>
-<option value='vibrant_ink'>Vibrant Ink</option>
+              <?php require("./skin.html")?>
 	  </select>
           <button id="changeskin" class="btn btn-default">更改皮肤</button>
         </div>
@@ -105,7 +70,6 @@
         </button>
         <br><br>
         <div class="form-group">
-          <input type="text" id="filename" placeholder="请输入此文件文件名......" class="form-control">
           <button class="btn btn-default" id="savecode">
             <span class="glyphicon glyphicon-save"></span>保存代码（通过Cookie）
           </button>
@@ -136,10 +100,10 @@
         var result = runcode(editor.getValue(),cin.getValue(), $("#language").val());
       });
       $("#savecode").click(function(){
-        $.cookie("File-" + $("#filename").val(), editor.getValue());
+        $.cookie("code", editor.getValue(),{ expires: 365});
       });
       $("#readcode").click(function(){
-        editor.setValue($.cookie("File-" + $("#filename").val()));
+        editor.setValue($.cookie("code"));
       });
     </script>
   </body>
